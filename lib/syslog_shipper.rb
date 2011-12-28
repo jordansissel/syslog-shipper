bfbea10f6425860ed76597368f9aa12309e464f0
Neil Matatall <neil@matatall.com>
2011-12-27 19:32:17 -0800

    Simple tis support, using EM's built in OpenSSL support
    This may suffice since there will only one connection.  Need to make certs configurable

--- lib/syslog_shipper.rb
+++ lib/syslog_shipper.rb
@@ -1,5 +1,6 @@
 require "eventmachine"
 require "eventmachine-tail"
 require File.expand_path('../syslog_shipper/client.rb',  __FILE__)
+require File.expand_path('../syslog_shipper/tls_wrapper.rb',  __FILE__)
 
 module SyslogShipper; end
