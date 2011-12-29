ca9d35400ab46e12d5f3cdbbdf9296bef8e537be
Neil Matatall <neil@matatall.com>
2011-12-29 13:21:12 -0800

    Adding test skeletons
    Yeah, I know, write the tests first…better than nothing

---
+++ features/step_definitions/client_steps.rb
@@ -0,0 +1,55 @@
+
+
+Given /^I have a valid endpoint$/ do
+  @port = 12345
+  @host_name = 'localhost'
+  # pending # express the regexp above with the code you wish you had
+end
+
+Given /^I have a valid TLS endpoint$/ do
+  SyslogShipper::Client.with_tls = true
+  SyslogShipper::Client.ca_cert = File.expand_path(File.dirname(__FILE__) + '../../test_certs/ca.crt')
+end
+
+Given /^I have an invalid TLS endpoint$/ do
+  SyslogShipper::Client.with_tls = true
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I connect to that endpoint$/ do
+  steps %Q{
+    When I run `syslog-shipper -s #{@host_name}:#{@port} *` interactively
+  }
+end
+
+When /^I securely connect to that endpoint$/ do
+  file_path = File.expand_path(File.dirname(__FILE__) + '../../test_certs/ca.crt')
+  steps %Q{
+    When I run `syslog-shipper --ca-cert #{file_path} -s #{@host_name}:#{@port} *` interactively
+  }
+end
+
+When /^I connect to that endpoint with and bypass peer checking$/ do
+  steps %Q{
+    When I run `syslog-shipper --skip-peer-check -s #{@host}:#{@port} *` interactively
+  }
+end
+
+When /^I am prompted to accept the certificate$/ do
+  steps %Q{
+    Then the output should contain "accept certificate"  
+  }
+end
+
+Then /^the connection should fail$/ do
+  steps %Q{
+    Then the output should contain "connection failed"  
+  }
+end
+
+
+Then /^I am not prompted to accept the certificate$/ do
+  steps %Q{
+    Then the output should not contain "accept certificate"  
+  }
+end
---
+++ spec/tls_wrapper_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe SyslogShipper::TlsWrapper do
+  context 'using a custom CA certificate' do
+
+  end
+
+  context 'connecting to an unverified peer' do
+
+  end
+
+  context 'connecting to a system verified peer' do
+
+  end
+end
