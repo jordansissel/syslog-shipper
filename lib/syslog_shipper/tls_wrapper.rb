6203d4e72faac90b906beb7e671b8a2b553fbe48
Neil Matatall <neil@matatall.com>
2012-01-09 14:17:53 -0800

    Don't prompt for verification on multiple threads

--- lib/syslog_shipper/tls_wrapper.rb
+++ lib/syslog_shipper/tls_wrapper.rb
@@ -1,47 +1,60 @@
 require 'openssl'
+require 'thread'
 
 module SyslogShipper::TlsWrapper
+  class << self
+    attr_accessor :verified
+  end
+
   def post_init
-    puts 'post init' if SyslogShipper::Client.verbose
+    puts 'post init'
+    @@semaphore ||= Mutex.new
     start_tls :verify_peer => SyslogShipper::Client.with_tls
   end
 
   def connection_completed
-    puts 'connection completed' if SyslogShipper::Client.verbose
+    puts 'connection completed'
   end
 
   def ssl_verify_peer cert
-    puts 'verifying peer' if SyslogShipper::Client.verbose
-    return true if SyslogShipper::Client.bypass_peer_check || $server_handshake_completed
+    puts 'verifying peer' 
+    @@semaphore.synchronize do
+      unless defined?(@@verified)
+        return true if SyslogShipper::Client.bypass_peer_check
 
-    server_cert = OpenSSL::X509::Certificate.new cert
-    verified = false
+        server_cert = OpenSSL::X509::Certificate.new cert
+        verified = false
 
-    if SyslogShipper::Client.ca_cert
-      ca_cert = read_ca_cert
-      verified = server_cert.verify(ca_cert.public_key)
-    end
+        if SyslogShipper::Client.ca_cert
+          ca_cert = read_ca_cert
+          verified = server_cert.verify(ca_cert.public_key)
+        end
 
-    unless verified
-      puts server_cert.inspect
-      print "The server certificate is not recognized, would you still like to connect? (Y/N) "
-      answer = STDIN.gets.chomp
-      unless answer =~ /y|yes/i
-        raise OpenSSL::X509::CertificateError.new("Couldn't verify peer")
+        unless verified
+          puts server_cert.inspect
+          print "The server certificate is not recognized, would you still like to connect? (Y/N) "
+          answer = STDIN.gets.chomp
+          unless answer =~ /y|yes/i
+            raise OpenSSL::X509::CertificateError.new("Couldn't verify peer")
+          end
+        end
+        
+        @@verified = verified
+
+        puts 'verified peer' if SyslogShipper::Client.verbose
       end
     end
-    
-    puts 'verified peer' if SyslogShipper::Client.verbose
+
     true
   end
 
   def ssl_handshake_completed
     puts 'ssl handshake completed' if SyslogShipper::Client.verbose
-    $server_handshake_completed = true
   end
 
   def unbind
-
+    exit(1)
+    puts 'connection unbound!'
   end
 
   private 
