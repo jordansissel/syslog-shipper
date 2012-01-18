290b13403b036804edbc79c86c287539afe6f17a
Neil Matatall <neil@matatall.com>
2012-01-17 18:11:59 -0800

    Give each Glob watcher a separate connection

--- lib/syslog_shipper/tls_wrapper.rb
+++ lib/syslog_shipper/tls_wrapper.rb
@@ -15,7 +15,7 @@ module SyslogShipper::TlsWrapper
 
   def post_init
     puts 'post init'
-    @@semaphore ||= Mutex.new
+    
     start_tls :verify_peer => @with_tls
   end
 
@@ -25,31 +25,29 @@ module SyslogShipper::TlsWrapper
 
   def ssl_verify_peer cert
     puts 'verifying peer' 
-    @@semaphore.synchronize do
-      unless defined?(@@verified)
-        return true if @bypass_peer_check
+    unless defined?(@@verified)
+      return true if @bypass_peer_check
 
-        server_cert = OpenSSL::X509::Certificate.new cert
-        verified = false
+      server_cert = OpenSSL::X509::Certificate.new cert
+      verified = false
 
-        if @ca_cert
-          ca_cert = read_ca_cert
-          verified = server_cert.verify(ca_cert.public_key)
-        end
-
-        unless verified
-          puts server_cert.inspect
-          print "The server certificate is not recognized, would you still like to connect? (Y/N) "
-          answer = STDIN.gets.chomp
-          unless answer =~ /y|yes/i
-            raise OpenSSL::X509::CertificateError.new("Couldn't verify peer")
-          end
-        end
-        
-        @@verified = verified
-
-        puts 'verified peer' if @verbose
+      if @ca_cert
+        ca_cert = read_ca_cert
+        verified = server_cert.verify(ca_cert.public_key)
       end
+
+      unless verified
+        puts server_cert.inspect
+        print "The server certificate is not recognized, would you still like to connect? (Y/N) "
+        answer = STDIN.gets.chomp
+        unless answer =~ /y|yes/i
+          raise OpenSSL::X509::CertificateError.new("Couldn't verify peer")
+        end
+      end
+      
+      @@verified = verified
+
+      puts 'verified peer' if @verbose
     end
 
     true
